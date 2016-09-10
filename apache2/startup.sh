117d0dcdb88e067e01a9c45598eb24cefbd4996f
tc <anthony.commander@gmail.com>
2016-09-10 10:37:06 +0100

    Initial Docker build

---
+++ apache2/startup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir /etc/apache2/ssl
+cd /etc/apache2/ssl
+openssl req \
+    -new \
+    -newkey rsa:2048 \
+    -days 365 \
+    -nodes \
+    -x509 \
+    -subj "/C=TC/ST=Everywhere/L=Internet/O=Development/CN=localhost" \
+    -keyout server.key \
+    -out server.cert
+
+service apache2 start
+tail -f /var/log/apache2/*
